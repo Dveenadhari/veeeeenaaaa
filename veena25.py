577fb01191673729f8fbbd5f3da11306e0b31528
Dveenadhari <41769824+Dveenadhari@users.noreply.github.com>
2018-08-11 15:40:24 +0530

    Create veena25.py

---
+++ veena25.py
@@ -0,0 +1,9 @@
+r=int(input("enter upper limit:"))
+for a in range(2,r+1):
+    k=o
+    for i in range(2,a//2+1):
+         if(a%1==0):
+              k=k+1
+         if(k<=0):
+            print(a)
+         
